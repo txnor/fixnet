cf4396b698742317cc37f16dd526a065866b5c4f
txnor <110873677+txnor@users.noreply.github.com>
2022-08-21 01:13:04 -0300

    Add files via upload

---
+++ fixnet.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+clear
+green="\e[1;32m"
+red="\e[31m"
+clear="\e[0m"
+echo -e "${green}
+
+░█▀▀▀ ▀█▀ ▀▄░▄▀ ░█▄─░█ ░█▀▀▀ ▀▀█▀▀ 
+░█▀▀▀ ░█─ ─░█── ░█░█░█ ░█▀▀▀ ─░█── 
+░█─── ▄█▄ ▄▀░▀▄ ░█──▀█ ░█▄▄▄ ─░█──
+${clear}
+"
+echo -e "${red}Script desenvolvido para melhorar a sua internet para cloud gaming, e navegação, etc.
+${clear}
+"
+echo -e "${green}
+Você já pode fazer o que quiser mas deixe o termuxrodando em segundo plano.${clear}"
+yes https://globe/international.speed.com > /dev/null 2>&1 &
